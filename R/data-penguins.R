81277a94a12a5bcf503cceaf9237294265cab226
nishta <nbol0011@student.monash.edu>
2025-11-04 18:01:52 +1100

    Document dataset: add penguins data documentation

---
+++ R/data-penguins.R
@@ -0,0 +1,17 @@
+#' Penguin measurements dataset
+#'
+#' This dataset contains body measurements for penguins observed in the Palmer Archipelago.
+#'
+#' @format A data frame with the following columns:
+#' \describe{
+#'   \item{species}{Species of penguin (Adelie, Chinstrap, Gentoo)}
+#'   \item{island}{Island where the penguin was observed}
+#'   \item{sex}{Male or Female}
+#'   \item{year}{Observation year}
+#'   \item{flipper_length_mm}{Flipper length (millimetres)}
+#'   \item{body_mass_g}{Body mass (grams)}
+#'   \item{bill_length_mm}{Bill length (millimetres)}
+#'   \item{bill_depth_mm}{Bill depth (millimetres)}
+#' }
+#' @source Adapted from the palmerpenguins dataset
+"penguins"
